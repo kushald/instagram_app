2ac254096f0bd6678155251c970f245a674d6d3b
Kushal Dongre <kushz19@gmail.com>
2013-05-26 13:20:47 +0530

    bug fixed for existing users - instagram image

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
     return {:err => 1} if params[:insta_id].blank?
     user = User.find_user(:insta_id => params[:insta_id])
     if user.present?
+      user.assign_image(params[:instagram_image]) if user.instagram_image.blank?
       user
     else
       User.create_user(params)
@@ -21,7 +22,15 @@ class User < ActiveRecord::Base
       :instagram_id => params[:insta_id],
       :instagram_access_token => params[:access_token],
       :instagram_username => params[:username],
-      :instagram_full_name => params[:full_name]
+      :instagram_full_name => params[:full_name],
+      :instagram_image => params[:instagram_image]
     )
+  end
+
+  def assign_image(instagram_image)
+    if self.instagram_image.blank?
+      self.instagram_image = instagram_image
+      self.save
+    end
   end
 end
