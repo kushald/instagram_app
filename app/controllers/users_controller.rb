48466d8e8e6ce2254d6870a80228ebe6e5760368
Kushal Dongre <kushz19@gmail.com>
2013-06-02 07:35:42 +0530

    adding relation content in following and followed by content

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,14 @@ class UsersController < ApplicationController
     instagram_id = params["id"] || @current_user.instagram_id
     @user_info = Request.get_request("https://api.instagram.com/v1/users/#{instagram_id}/?access_token=#{@current_user.instagram_access_token}")
     @data = Request.get_request("https://api.instagram.com/v1/users/#{instagram_id}/follows?access_token=#{@current_user.instagram_access_token}")
+    @relationship = Request.get_request("https://api.instagram.com/v1/users/#{params[:id]}/relationship?access_token=#{@current_user.instagram_access_token}")
   end
 
   def followed_by
     instagram_id = params["id"] || @current_user.instagram_id
     @user_info = Request.get_request("https://api.instagram.com/v1/users/#{instagram_id}/?access_token=#{@current_user.instagram_access_token}")
     @data = Request.get_request("https://api.instagram.com/v1/users/#{instagram_id}/followed-by?access_token=#{@current_user.instagram_access_token}")
+    @relationship = Request.get_request("https://api.instagram.com/v1/users/#{params[:id]}/relationship?access_token=#{@current_user.instagram_access_token}")
   end
 
   def my_pics
