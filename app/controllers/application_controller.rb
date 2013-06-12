950e9d72a0d1cd71c62b21b389ee55af90bae207
Kushal Dongre <kushz19@gmail.com>
2013-06-12 20:24:25 +0530

    redirect to original site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :current_user
   private
   def current_user
+    redirect_to "http://www.joystagram.com"
     if cookies["ac"].present?
       @current_user ||= User.where(:id => cookies["ac"].split("a12b")[1]).first 
     else
