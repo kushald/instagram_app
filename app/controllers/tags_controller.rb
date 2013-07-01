caf0352fd044d5349ab5ac788e32653a8d55728d
Kushal Dongre <kushz19@gmail.com>
2013-07-01 11:30:09 +0530

    tag exception solved

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,6 +6,5 @@ class TagsController < ApplicationController
 
   def show
     @data = Request.get_request("https://api.instagram.com/v1/tags/#{params[:id]}/media/recent?access_token=#{@current_user.instagram_access_token}")
-    @relationship = Request.get_request("https://api.instagram.com/v1/users/#{params[:id]}/relationship?access_token=#{@current_user.instagram_access_token}")
   end
 end
