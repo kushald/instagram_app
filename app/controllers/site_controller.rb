ba9f9231a84d56a5d0d88c2d5d74100f3c31b9f4
Kushal Dongre <kushz19@gmail.com>
2013-07-10 12:23:17 +0530

    home page to show category 2 in random

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -9,7 +9,7 @@ class SiteController < ApplicationController
   end
 
   def index
-    @interesting_users = InterestingUser.where(:category_type => 1).all.sample(8).index_by(&:instagram_user_id)
+    @interesting_users = InterestingUser.where(:category_type => [1,2].sample).all.sample(8).index_by(&:instagram_user_id)
     #redirect_to "/popular" if @interesting_users.blank?
     @interesting_user_posts = InterestingUserPost.where(:instagram_user_id => @interesting_users.keys).all.shuffle.group_by(&:instagram_user_id)
   end
