6fcf08e676334a02881315dcbc1d385740979a50
Kushal Dongre <kushz19@gmail.com>
2013-08-22 00:28:48 +0530

    httpcache

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,9 +1,11 @@
 class MediaController < ApplicationController
   def likes
     @likes = Request.get_request("https://api.instagram.com/v1/media/#{params[:id]}/likes?access_token=#{@current_user.instagram_access_token}")
+    expires_in 5.minutes, :public => true
   end
 
   def comments
     @comments = Request.get_request("https://api.instagram.com/v1/media/#{params[:id]}/comments?access_token=#{@current_user.instagram_access_token}")
+    expires_in 5.minutes, :public => true
   end
 end
