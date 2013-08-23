16c0d6bc918cb16c57b5e59325e1e34e299e2869
Kushal Dongre <kushz19@gmail.com>
2013-08-24 03:30:43 +0530

    last child float left for columns

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,6 +6,5 @@ class TagsController < ApplicationController
 
   def show
     @data = Request.get_request("https://api.instagram.com/v1/tags/#{params[:id]}/media/recent?access_token=#{@current_user.instagram_access_token}&max_tag_id=#{params[:n]}")
-    expires_in 5.minutes, :public => true
   end
 end
