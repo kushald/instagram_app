388cf7048df015600e56af6b1cb30354c66e0731
Kushal Dongre <kushz19@gmail.com>
2013-08-24 02:35:06 +0530

    no tag count and layout change

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,7 +5,7 @@ class TagsController < ApplicationController
   end
 
   def show
-    @data = Request.get_request("https://api.instagram.com/v1/tags/#{params[:id]}/media/recent?access_token=#{@current_user.instagram_access_token}&max_tag_id=#{params[:n]}&count=10")
+    @data = Request.get_request("https://api.instagram.com/v1/tags/#{params[:id]}/media/recent?access_token=#{@current_user.instagram_access_token}&max_tag_id=#{params[:n]}")
     expires_in 5.minutes, :public => true
   end
 end
