64e0c53eb80c24b0ea6707115108b5de85fbfb2d
Kushal Dongre <kushz19@gmail.com>
2013-09-06 08:37:10 +0530

    class variable for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ActiveRecord::Base
   attr_accessible :description, :name, :image
+  @@categories = []
+  
+  
+  def self.get_categories
+    @@categories = all if @@categories.blank?
+
+    @@categories
+  end
 end
