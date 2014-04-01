8f257ef1e8a91b08fc0a50d08f0b5832d7706486
Kushal Dongre <kushz19@gmail.com>
2014-04-02 00:04:40 +0530

    valeryparadise

--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -1,6 +1,6 @@
 class TrendingController < ApplicationController
   def index
-    @trending = Trending.page(params[:page]).per(20)
+    @trending = Trending.order("id DESC").page(params[:page]).per(20)
     expires_in 4.minutes, :public => true    
   end
 end
--- lib/constant.rb
+++ lib/constant.rb
@@ -31,9 +31,9 @@ module Constant
                      ]
 
   EXCLUDE_USER_IDS = ["361460824", "179293483", "509794020", "27014377", "250566440", "376601127", "30768248", "4350221", 
-                      "182884386", "223901729", "36476395"]
+                      "182884386", "223901729", "36476395", "180298319"]
   EXCLUDE_USER_NAMES = ["helloojaclyn", "im5xoxo", "akshaykharb", "gkriebel9899", "myajeannine", "jessimicablaze",
-                         "sofieaaboe96", "jamieleecreations", "jacicutiee", "hugoblc", "janatovalovich_art", "cameliasaltos"]
+                         "sofieaaboe96", "jamieleecreations", "jacicutiee", "hugoblc", "janatovalovich_art", "cameliasaltos", "valeryparadise"]
 
   TEMP_TOP_BRANDS = [
                       {:display => "American Apparel USA", :url => "americanapparelusa"},
